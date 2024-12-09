f6f649fe3a42d1930304c2ee15a462322e5cdb1f
guoh0025 <GUOH0025@e.ntu.edu.sg>
2024-12-09 12:09:38 +0000

    Finally!!

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask
 from flask import render_template,request
 import textblob
 import os
+import google.generativeai as genai
 
-#api = os.getenv("maskersuiter")
+api = os.getenv("makersuite")
+
+genai.configure(api_key = "AIzaSyAOnPNbTckppvB4wvnYPjFiK9fNZQHT7QQ")
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+
 
 app = Flask(__name__)
 
@@ -19,6 +25,15 @@ def main():
 @app.route("/GenAI",methods=["GET","POST"])
 def GenAI():
     return(render_template("GenAI.html"))
+
+@app.route("/GAI",methods=["GET","POST"])
+def GAI():
+    return(render_template("GAI.html"))
+@app.route("/GAI_result",methods=["GET","POST"])
+def GAI_result():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("GAI_result.html", r=r.candidates[0].content.parts[0].text))
 
 @app.route("/SA",methods=["GET","POST"])
 def SA():
